383a7ddbcb891a2bdabbffaad33bbd68e9b31183
Shawn Ellis <seellis1@student.fullsail.edu>
2025-03-02 22:11:43 -0500

    Coding complete, refactor/ fine tuning in progress

--- main.js
+++ main.js
@@ -46,10 +46,12 @@ class Main {
     //Choice Menu for User
     showMenu() {
         while (true){
-            console.clear();
-            let choice = prompt("Choose an option: \n1. Add Employee\n2. Remove Employee\n3. Edit Employee\n4. Display Employees\n5. Exit");
-            if (choice === "5") break;
-            this.handleChoice(choice);
+            let choice = prompt("Main Menu \n 1. Add Employee\n2. Remove Employee\n3. Edit Employee\n4. Display Employees \n \nEnter Selection:");
+            if (choice === "1" || choice === "2" || choice === "3" || choice === "4"){
+                this.handleChoice(choice);
+            } else if (choice !== "1" && choice !== "2" && choice !== "3" && choice !== "4"){
+                alert("Please select the options presented to you.");
+            }
         }
     }
 
@@ -78,8 +80,36 @@ class Main {
         console.log("Employee successfully added!");
         this.displayEmployees();
     }
-}
 
+    removeEmployee() {
+        let name = prompt("Enter employee name to remove:");
+        this.employees = this.employees.filter(remove => remove.name !== name);
+        console.log("Employee successfully removed.");
+        this.displayEmployees();
+    }
+
+    editEmployee() {
+        let name = prompt("Enter employee name to edit:");
+        let employee = this.employees.filter(edit => edit.name === name);
+        if (employee) {
+            employee.payRate = parseFloat(prompt("Enter new pay rate:"));
+            employee.calculatePay();
+            console.log("Employee pay rate updated!")
+        } else {
+            alert("Employee not found. Enter an existing employee.");
+            console.log("Employee not found.");
+        }
+        this.displayEmployees();
+    }
+
+    displayEmployees() {
+        console.log("Shawn's Pizza Place");
+        console.log("ID\tName\tAge\tSalary\Hours\tPay\tFT/PT");
+        this.employees.forEach((emp, index) => {
+            console.log(`${index + 1}\t${emp.name}\t${emp.age}\t${emp.employeeSalary}\t${emp.hours || 40}\t${emp.payRate}\t${emp.employeeType}`);
+        });
+    }
+}
 
 // IIFE
 (function() {
